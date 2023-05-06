48544286808278e6f1d8a13683a4c9b440ec2c98
Ana-Ma-Web <90105049+Ana-Ma-Web@users.noreply.github.com>
2023-05-06 09:27:36 +0300

    feat: add 04-copy-directory task

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,1 +1,40 @@
+const { mkdir, copyFile, readdir, unlink } = require('node:fs/promises');
+const { join } = require('node:path');
 
+const makeDirectory = async () => {
+
+  try {
+    const copyFolder = join(__dirname, 'files-copy');
+    const srcFiles = await readdir(join(__dirname, 'files'));
+
+    const createdFolder = async (copyFolder) => {
+      await mkdir(copyFolder, { recursive: true });
+      return copyFolder;
+    };
+
+    const createdFolderPath = await createdFolder(copyFolder);
+    const clearFolder = async (copyFolder) => {
+      const copyFiles = await readdir(
+        join(createdFolderPath)
+      );
+
+      for (const file of copyFiles) {
+        await unlink(join(createdFolderPath, file));
+      }
+      return copyFolder;
+    };
+
+    const clearFolderPath = await clearFolder(copyFolder);
+
+    for (const file of srcFiles) {
+      copyFile(
+        join(__dirname + '/files/' + file),
+        join(clearFolderPath, file)
+      );
+    }
+  } catch (err) {
+    console.error(err);
+  }
+};
+
+makeDirectory();
